be84ccccdc414c664ed1c6dc7956d650eec48686
Agnieszka H.-K <agnieszka.krawczyk@seznam.cz>
2019-05-06 22:00:37 +0200

    solved

---
+++ Accounts/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    class Account
+    {
+        public string accNumber;
+        public string accOwner;
+        public double accBalance;
+
+        public Account(string accountNumber, string accountOwner)
+        {
+            accNumber = accountNumber;
+            accOwner = accountOwner;
+        }
+
+        public void Increase(double amount)
+        {
+            accBalance += amount;
+        }
+
+        public bool Decrease(double amount)
+        {
+            if (accBalance >= amount)
+            {
+                accBalance -= amount;
+                return true;
+
+            }
+            else
+                return false;
+        }
+    }
+}
--- Accounts/Form1.cs
+++ Accounts/Form1.cs
@@ -16,5 +16,23 @@ namespace Accounts
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Account petersAccount = new Account("123457/8765", "Peter");
+
+            petersAccount.Increase(2000);
+
+            bool firstWithdrawal = petersAccount.Decrease(500);
+            bool secondWithdrawal = petersAccount.Decrease(5000);
+
+            MessageBox.Show(
+                "Account Number: " + petersAccount.accNumber + Environment.NewLine +
+                "Account Owner: " + petersAccount.accOwner + Environment.NewLine +
+                "1st Withdrawal: " + firstWithdrawal.ToString() + Environment.NewLine +
+                "2nd Withdrawal: " + secondWithdrawal.ToString() + Environment.NewLine +
+                "Total Balance: " + petersAccount.accBalance.ToString() + Environment.NewLine
+                );
+        }
     }
 }
