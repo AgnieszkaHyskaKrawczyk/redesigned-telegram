0dd8070f220784cb5e1eb9be216663e05ee57b82
Agnieszka H.-K <agnieszka.krawczyk@seznam.cz>
2019-05-07 23:19:33 +0200

    adjusted

--- Accounts/Account.cs
+++ Accounts/Account.cs
@@ -8,9 +8,25 @@ namespace Accounts
 {
     class Account
     {
-        public string accNumber;
-        public string accOwner;
-        public double accBalance;
+        private string accNumber;
+        public string AccNumber
+        {
+            get { return accNumber; }
+        }
+
+        private string accOwner;
+        public string AccOwner
+        {
+            get { return accOwner; }
+            set { accOwner = value; }
+        }
+
+        private double accBalance = 0;
+        public double AccBalance
+        {
+            get { return accBalance; }
+            private set { accBalance = value; }
+        }
 
         public Account(string accountNumber, string accountOwner)
         {
